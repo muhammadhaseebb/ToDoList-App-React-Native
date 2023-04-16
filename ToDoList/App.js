13914dfe3f873ead09a64fcba7bd66ba53a2ee23
Muhammad Haseeb <104450111+muhammadhaseebb@users.noreply.github.com>
2023-04-16 22:53:30 +0500

    final

--- ToDoList/App.js
+++ ToDoList/App.js
@@ -5,114 +5,139 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
+import {TextInput, Button} from 'react-native-paper';
+
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [press, setPress] = useState();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleNewTodo = () => {
+    if (newTodo.trim() === '') return;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    setTodos([...todos, newTodo]);
+    setNewTodo('');
+  };
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const deletetask = index => {
+    let todoscopy = [...todos];
+    todoscopy.splice(index, 1);
+    setTodos(todoscopy);
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const updatetask = (index, newtxt) => {
+    setPress(index);
+    setNewTodo(newtxt);
+  };
+
+  const handleupdate = () => {
+    let todoscopy = [...todos];
+    todoscopy[press] = newTodo;
+    setTodos(todoscopy);
+    setNewTodo('');
+    setPress();
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <View style={{backgroundColor: 'white', flex: 1}}>
+      <StatusBar backgroundColor={'black'} />
+
+      {/* textfield and button */}
+      <View
+        style={{
+          backgroundColor: 'black',
+          alignItems: 'center',
+          height: 60,
+          justifyContent: 'center',
+        }}>
+        <Text style={{fontSize: 32, fontFamily: 'Billabong', paddingTop: 20}}>
+          To Do List
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          margin: 20,
+        }}>
+        <TextInput
+          style={{width: '60%'}}
+          mode="outlined"
+          label="Enter an item"
+          value={newTodo}
+          onChangeText={text => setNewTodo(text)}
+          right={<TextInput.Icon icon={'archive-arrow-up'} />}
+        />
+        <Button
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+            backgroundColor: 'black',
+            height: 40,
+            paddingHorizontal: 20,
+          }}
+          onPress={press !== undefined ? handleupdate : handleNewTodo}
+          textColor="white">
+          {press !== undefined ? 'Update' : 'Add Item'}
+        </Button>
+      </View>
+
+      {/* Output tiles */}
+      <ScrollView>
+        {todos.map((todo, index) => (
+          <View
+            key={index}
+            style={{
+              borderWidth: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              marginHorizontal: 20,
+              marginBottom: 10,
+              borderRadius: 10,
+              paddingVertical: 10,
+            }}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Text
+                style={{
+                  color: 'white',
+                  marginHorizontal: 20,
+                  backgroundColor: 'black',
+                  width: 20,
+                  height: 20,
+                  textAlign: 'center',
+                }}>
+                {index + 1}
+              </Text>
+              <TouchableOpacity
+                key={index}
+                onPress={() => updatetask(index, todo)}>
+                <Text
+                  key={index}
+                  style={{
+                    color: 'black',
+                    fontSize: 16,
+                    maxWidth: 190,
+                  }}>
+                  {todo}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Button
+              icon="delete"
+              key={index}
+              onPress={() => deletetask(index)}></Button>
+          </View>
+        ))}
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
